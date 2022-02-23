4e0b3f8a8a3dab941ac48b560779c564947c7bc9
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-02-23 01:01:50 -0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Prevents the following CSP-related exception for development build:
+    //  Uncaught EvalError: Refused to evaluate a string as JavaScript because 'unsafe-eval' is not an
+    //  allowed source of script in the following Content Security Policy directive: "script-src 'self'
+    //  blob: filesystem: chrome-extension-resource:".
+    devtool: "none"
+};
